6f424e94d2eb5f9569b60b5f7f2b30688ca51f96
RocBambino <83316065+RocBambino@users.noreply.github.com>
2023-01-02 14:19:15 -0500

    Pokeon Buff

--- cogs/lookup.py
+++ cogs/lookup.py
@@ -878,7 +878,7 @@ class Lookup(commands.Cog):
                 summon_page = discord.Embed(title="Family Summon", description=textwrap.dedent(f"""
                 🧬**Family Summon**
                 """), colour=0x7289da)
-                summon_page.set_image(summon_img)
+                summon_page.set_image(url=summon_img)
                 
                 
                 
