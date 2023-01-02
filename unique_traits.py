78b8ac191b7922ada7e70424201d0a8bcf164de1
RocBambino <83316065+RocBambino@users.noreply.github.com>
2023-01-01 19:56:51 -0500

    Pokeon Buff

--- unique_traits.py
+++ unique_traits.py
@@ -1,98 +1,98 @@
 traits = [{
     'NAME': 'My Hero Academia',
     'EFFECT': 'Plus Ultra!',
-    'TRAIT': '\nYou do not lose a turn after you Resolve. AP of all abilities is increased by 200 + Turn Count.'
+    'TRAIT': '*Resolve*\n**Plus Ultra**: You do not lose a turn after you Resolve. AP of all abilities is increased by 200 + Turn Count.'
 },{
     'NAME': 'League Of Legends',
     'EFFECT': 'Turret Shot, Pentakill',
-    'TRAIT': '\n**Turret Shot**: When you Focus you deal (60 + Turn Count) Damage to Opponent. \n**Pentakill**: Upon Resolve, you deal 60 x (Total Focus Count) Damage.'
+    'TRAIT': '*Focus*,*Resolve*\n**Turret Shot**: When you Focus you deal (60 + Turn Count) Damage to Opponent. \n**Pentakill**: Upon Resolve, you deal 60 x (Total Focus Count) Damage.'
 },{
     'NAME': 'Pokemon',
     'EFFECT': 'Regions, Evolutions',
-    'TRAIT': '\n**Regions**: Titles and Arms from Pokemon Universes are shared. \n**Evolutions**: When you Resolve your Defense Doubles. \n**Mega Evolution**: If Turn >30 grow 500 HP, \n**Gigantomax**: If Turn >50 grow 1000 HP.'
+    'TRAIT': '*Equipment*,*Resolve*\n**Regions**: Titles and Arms from Pokemon Universes are shared. \n**Evolutions**: When you Resolve your Defense Doubles. \n**Mega Evolution**: If Turn >30 grow 500 HP, \n**Gigantomax**: If Turn >50 grow 1000 HP.'
 },{
     'NAME': 'Digimon',
     'EFFECT': 'Digivolve, MegaDigivolution',
-    'TRAIT': '\n**Digivolve**:On Focus you Resolve gain 50% ATK and DEF. \n**Mega Digivolution**: If turn count <5 Double ATK and DEF increase Health by 500.'
+    'TRAIT': '*Focus*,*Focus*\n**Digivolve**:On Focus you Resolve gain 50% ATK and DEF. \n**Mega Digivolution**: If turn count <5 Double ATK and DEF increase Health by 500.'
 },{
     'NAME': 'Naruto',
     'EFFECT': 'Substitution, Hashirama Cells',
-    'TRAIT': '\n**Substitution**: If Focused, you cannot be hit. \n**Hashirama Cells**: On Resolve, Hashirama Cells heal from Damage stored during Substitution.'
+    'TRAIT': '*Focus*,*Resolve*\n**Substitution**: If Focused, you cannot be hit. \n**Hashirama Cells**: On Resolve, Hashirama Cells heal from Damage stored during Substitution.'
 },{
     'NAME': 'Chainsawman',
     'EFFECT': 'Devilization, Fearful',
-    'TRAIT': '\n**Devilization**: When Health reaches 50% gain Double ATK, Double DEF, and Double Max Health. \n**Fearful**: Strong Fear Infliction.'
+    'TRAIT': '*Health*,*Equipment*\n**Devilization**: When Health reaches 50% gain Double ATK, Double DEF, and Double Max Health. \n**Fearful**: Strong Fear Infliction.'
 },{
     'NAME': 'Bleach',
     'EFFECT': 'Bankai, Spiritual Pressure',
-    'TRAIT': '\n**Bankai**: Gain Double Attack on Resolve. \n**Spiritual Pressure**: On Block, exert your Spiritual Pressure to deal damage equal to your basic attack while still gaining defense.'
+    'TRAIT': '*Resolve*,*Block*\n**Bankai**: Gain Double Attack on Resolve. \n**Spiritual Pressure**: On Block, exert your Spiritual Pressure to deal damage equal to your basic attack while still gaining defense.'
 },{
     'NAME': 'Attack On Titan',
     'EFFECT': 'Rally, Titan Mode',
-    'TRAIT': '\n**Rally**: Gain 100 Max Health and 50 Health on Block. \n**Titan Mode**: Gain (100 x Your Focus Count) worth of Health upon Resolve.'
+    'TRAIT': '*Block*,*Resolve*\n**Rally**: Gain 100 Max Health and 50 Health on Block. \n**Titan Mode**: Gain (100 x Your Focus Count) worth of Health upon Resolve.'
 },{
     'NAME': 'Black Clover',
     'EFFECT': 'Mana Zone',
-    'TRAIT': '\n**Mana Zone**: Gain 100 Stamina on Focus. AP is increased by 30 on Focus.'
+    'TRAIT': '*Focus*\n**Mana Zone**: Gain 100 Stamina on Focus. AP is increased by 30 on Focus.'
 },{
     'NAME': 'Fate',
     'EFFECT': 'Command Seal',
-    'TRAIT': '\n**Command Seal**: On Resolve, Strike with Ultimate with no Stamina cost.'
+    'TRAIT': '*Resolve*\n**Command Seal**: On Resolve, Strike with Ultimate with no Stamina cost.'
 },{
     'NAME': 'Demon Slayer',
     'EFFECT': 'Total Concentration Breathing, Total Concentration Constant',
-    'TRAIT': '\n**Breathing** At match start gain 40% of Opponent HP as your own. \n**Constant** On Resolve, Your ATK & DEF = Opp ATK and DEF if Opp stats are greater.'
+    'TRAIT': '*Passive*,*Resolve*\n**Breathing** At match start gain 40% of Opponent HP as your own. \n**Constant** On Resolve, Your ATK & DEF = Opp ATK and DEF if Opp stats are greater.'
 },{
     'NAME': 'God Of War',
     'EFFECT': 'Blood Orb, Ascension',
-    'TRAIT': ' \nYou can Resolve twice. \n**Blood Orb **: 1st Resolve heal half of max health. \n**Ascension**: 2nd Resolve heal full max health.'
+    'TRAIT': '*Resolve*,*Resolve*\nYou can Resolve twice. \n**Blood Orb **: 1st Resolve heal half of max health. \n**Ascension**: 2nd Resolve heal full max health.'
 },{
     'NAME': '7ds',
     'EFFECT': 'Power Of Friendship',
-    'TRAIT': '\n**Power Of Friendship**: On opponent Focus gain 60 Stamina and Summon Rest.'
+    'TRAIT': '*Focus*\n**Power Of Friendship**: On opponent Focus gain 60 Stamina and Summon Rest.'
 },{
     'NAME': 'One Punch Man',
     'EFFECT': 'Hero Reinforcement',
-    'TRAIT': '\n**Hero Reinforcement**: When Opponent Focuses, you gain 100 Health & Max Health.'
+    'TRAIT': '*Focus*\n**Hero Reinforcement**: When Opponent Focuses, you gain 100 Health & Max Health.'
 },{
     'NAME': 'Solo Leveling',
     'EFFECT': 'Rulers Authority, Arise!',
-    'TRAIT': '\n**Rulers Authority**: On Focus, opponent loses (30 + turn count) Defense. \n**Arise!**: When Opponent Arm Breaks it revives as your Arm.'
+    'TRAIT': '*Focus*,*Equipment*\n**Rulers Authority**: On Focus, opponent loses (30 + turn count) Defense. \n**Arise!**: When Opponent Arm Breaks it revives as your Arm.'
 },{
     'NAME': 'Souls',
     'EFFECT': 'Combo Recognition, Second Phase!',
-    'TRAIT': '\n**Combo Recognition**: On opponent Focus, gain (60 + turn count) Attack. \n**Second Phase!**: On Resolve, BASIC becomes your SPECIAL, and your SPECIAL becomes another ULTIMATE.'
+    'TRAIT': '*Focus*,*Resolve*\n**Combo Recognition**: On opponent Focus, gain (60 + turn count) Attack. \n**Second Phase!**: On Resolve, BASIC becomes your SPECIAL, and your SPECIAL becomes another ULTIMATE.'
 },{
     'NAME': 'Death Note',
     'EFFECT': 'Scheduled Death',
-    'TRAIT': '\n**Scheduled Death**: Your First Focus After 60 Turns You Win.'
+    'TRAIT': '*Turn Count*\n**Scheduled Death**: Your First Focus After 60 Turns You Win.'
 },{
     'NAME': 'Dragon Ball Z',
     'EFFECT': 'Final Stand, Saiyan Spirit',
-    'TRAIT': '\n**Final Stand**:  When you die, Resolve with 75% ATK + DEF as Health. \n**Saiyan Spirit!**:  On focus, heal for opponent Stamina amount + turn count.'
+    'TRAIT': '*Death*,*Focus*\n**Final Stand**:  When you die, Resolve with 75% ATK + DEF as Health. \n**Saiyan Spirit!**:  On focus, heal for opponent Stamina amount + turn count.'
 },{
     'NAME': 'Persona',
     'EFFECT': 'Summon Persona!',
-    'TRAIT': '\n**Summon Persona!**: Summon deals Damage equal to your Basic attack, in addition to their status effect, disabling opponents summon. '
+    'TRAIT': '*Summon*\n**Summon Persona!**: Summon deals Damage equal to your Basic attack, in addition to their status effect, disabling opponents summon. '
 },{
     'NAME': 'YuYu Hakusho',
     'EFFECT': 'Spirit Energy',
-    'TRAIT': '\n**Spirit Energy**: Stamina + Turn Count added to each attack | Attack increased by Stamina each turn.'
+    'TRAIT': '*Stamina*\n**Spirit Energy**: Stamina + Turn Count added to each attack | Attack increased by Stamina each turn.'
 },
 {
     'NAME': 'One Piece',
     'EFFECT': 'Observcation Haki, Armament Haki, Conquerors Haki',
-    'TRAIT': '\n**Observation Haki**: mahjong:1-3 Before first focus all opponent dmg reduced by 60%\n**Armament Haki**: mahjong:4-5 Observaton Haki + Double defense &  attack gain on Focus, every focus\n**Conquerors Haki**: Armament Haki + Decrease opponent moves AP by 125 on Resolve'
+    'TRAIT': '*Focus*,*Focus*,*Resolve*\n**Observation Haki**: :mahjong:1-3 Before first focus all opponent dmg reduced by 60%\n**Armament Haki**: :mahjong:4-5 Observaton Haki + Double defense &  attack gain on Focus, every focus\n**Conquerors Haki**: :mahjong:6-7 Armament Haki + Decrease opponent moves AP by 125 on Resolve'
 },{
     'NAME': 'Crown Rift Slayers',
     'EFFECT': 'Feint Attack',
-    'TRAIT': '\n**Feint Attack**: If you miss, Crit!'
+    'TRAIT': '*Attack*\n**Feint Attack**: If you miss, Crit!'
 },{
     'NAME': 'Crown Rift Madness',
     'EFFECT': 'Madness',
-    'TRAIT': '\n**Madness**: Do not lose turn on Focus, but also do not Heal.'
+    'TRAIT': '*Focus*\n**Madness**: Do not lose turn on Focus, but also do not Heal.'
 },{
     'NAME': 'Crown Rift Awakening',
     'EFFECT': 'Blood Awakening, Spiritual Weapons',
-    'TRAIT': '\n**Blood Awakening**: Deal 4x damage on Critical Strikes, Increased Crit Chance. \n**Spiritual Weapons**: Can use Arms from all Universes'
+    'TRAIT': '*Attack*,*Equipment*\n**Blood Awakening**: Deal 4x damage on Critical Strikes, Increased Crit Chance. \n**Spiritual Weapons**: Can use Arms from all Universes'
 }]
