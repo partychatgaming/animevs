0c183c8eea3984c28e210a27da4adbcf8eed8d84
RocBambino <83316065+RocBambino@users.noreply.github.com>
2023-01-04 00:38:47 -0500

    Family Update

--- bot.py
+++ bot.py
@@ -848,6 +848,8 @@ async def register(ctx):
       name = disname.split("#",1)[0]
       family = db.createFamily(data.newFamily({'HEAD': str(disname), 'SUMMON': {'NAME': 'Chick', 'LVL': 1, 'EXP': 0, 'Heal': 5, 'TYPE': 'HLT', 'BOND': 0, 'BONDEXP': 0,
          'PATH': "https://res.cloudinary.com/dkcmq8o15/image/upload/v1638814575/Pets/CHICK.png"}}), str(disname))
+      update_summon = db.updateFamily({'HEAD': str(family['HEAD'])}, {'$set' : {'SUMMON': {'NAME': 'Chick', 'LVL': 1, 'EXP': 0, 'Heal': 5, 'TYPE': 'HLT', 'BOND': 0, 'BONDEXP': 0,
+         'PATH': "https://res.cloudinary.com/dkcmq8o15/image/upload/v1638814575/Pets/CHICK.png"}}})
       user = {'DISNAME': disname, 'NAME': name, 'DID' : str(ctx.author.id), 'AVATAR': str(ctx.author.avatar_url), 'SERVER': str(ctx.author.guild), 'FAMILY': str(disname)}
       r_response = db.createUsers(data.newUser(user))
 
