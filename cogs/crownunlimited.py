168fd6452c331e3ea4a0e423ea1b510dfa3cfbd7
RocBambino <83316065+RocBambino@users.noreply.github.com>
2023-03-04 21:11:54 -0500

    checking for dbz final stand

--- cogs/crownunlimited.py
+++ cogs/crownunlimited.py
@@ -2266,7 +2266,7 @@ async def select_universe(self, ctx, p: object, mode: str, p2: None):
         def check(button_ctx):
             print(button_ctx.author.id )
             print(user2)
-            return button_ctx.author.id == user2
+            return button_ctx.author == user2
         try:
             button_ctx: ComponentContext = await manage_components.wait_for_component(self.bot, components=[coop_buttons_action_row], timeout=120, check=check)
 
