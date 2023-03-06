aa72058080e83af5be077f233dc60cc203b73d99
RocBambino <83316065+RocBambino@users.noreply.github.com>
2023-03-05 21:29:47 -0500

    checking for dbz final stand

--- cogs/crownunlimited.py
+++ cogs/crownunlimited.py
@@ -3705,9 +3705,9 @@ async def battle_commands(self, ctx, battle_config, _player, _custom_explore_car
                                     embedVar = await auto_battle_embed_and_starting_traits(ctx, player2_card, player3_card, battle_config, None)
                                     if player3.performance:
                                         embedVar.add_field(name=f":sunny: **Move Set**", value=f"{player2_card.move1_emoji}{player2_card.move1ap}\n{player2_card.move2_emoji}{player2_card.move2ap}\n{player2_card.move3_emoji}{player2_card.move3ap}\n:microbe:{player2_card.move4enh} {player2_card.move4ap}")
-                                        battle_msg = await private_channel.send(embed=embedVar, components=components)
+                                        battle_msg = await private_channel.send(embed=embedVar)
                                     else:
-                                        battle_msg = await private_channel.send(embed=embedVar, file=player2_card.showcard(battle_config.mode, player2_arm, player2_title, battle_config.turn_total, player3_card.defense))
+                                        battle_msg = await private_channel.send(embed=embedVar,file=player2_card.showcard(battle_config.mode, player2_arm, player2_title, battle_config.turn_total, player3_card.defense))
 
 
                                     selected_move = battle_config.ai_battle_command(player2_card, player3_card)
