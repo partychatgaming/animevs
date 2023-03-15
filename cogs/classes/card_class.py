53e9e31291f496b40f187a99bb6cf3d0d75cfacb
RocBambino <tillmanholdingsllc@gmail.com>
2023-03-15 14:55:18 -0400

    Update card_class.py

--- cogs/classes/card_class.py
+++ cogs/classes/card_class.py
@@ -1900,11 +1900,7 @@ class Card:
             self.usedsummon = False
             self.focus_count = self.focus_count + 1
             #self.max_health = self.max_health + self._heal_value
-            if self._heal_active:
-                self.health = self.health + self._heal_value
-                if self.health >= self.max_health:
-                    self.health = self.max_health
-                self._heal_value = 0
+            
 
             if battle_config.is_boss_game_mode and battle_config.is_turn not in [1,3]:
                 embedVar = discord.Embed(title=f"{battle_config._punish_boss_description}")
@@ -1924,6 +1920,11 @@ class Card:
 
             self.stamina = self.stamina_focus_recovery_amount
             health_calculation = round(fortitude)
+            if self._heal_active:
+                health_calculation = health_calculation + self._heal_value
+                if self.health >= self.max_health:
+                    self.health = self.max_health
+                self._heal_value = 0
             attack_calculation = round((fortitude * (self.tier / 10)) + (.05 * self.attack))
             defense_calculation = round((fortitude * (self.tier / 10)) + (.05 * self.defense))
             
