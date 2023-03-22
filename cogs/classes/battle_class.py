57c5ac2cc1da3774598cf72d1490b2d40c06a225
christopher simmons <kadensdad93@gmail.com>
2023-03-21 23:41:41 -0700

    updated scenario selection

--- cogs/classes/battle_class.py
+++ cogs/classes/battle_class.py
@@ -547,8 +547,21 @@ class Battle:
                         embed_list.append(embedVar)
 
             return embed_list
-        except:
-            print("Error setting scenario selection config")
+        except Exception as ex:
+            trace = []
+            tb = ex.__traceback__
+            while tb is not None:
+                trace.append({
+                    "filename": tb.tb_frame.f_code.co_filename,
+                    "name": tb.tb_frame.f_code.co_name,
+                    "lineno": tb.tb_lineno
+                })
+                tb = tb.tb_next
+            print(str({
+                'type': type(ex).__name__,
+                'message': str(ex),
+                'trace': trace
+            }))
 
 
     def set_scenario_config(self, scenario_data):
