1107b85b0adcbb61ef2ee223112e7375d5a72be3
RocBambino <83316065+RocBambino@users.noreply.github.com>
2023-03-28 02:07:55 -0400

    trait list fixed

--- unique_traits.py
+++ unique_traits.py
@@ -5,15 +5,15 @@ traits = [{
 },{
     'NAME': 'League Of Legends',
     'EFFECT': 'Turret Shot, Pentakill',
-    'TRAIT': 'Turret Shot: On Focus you deal up to 15% Damage + Turn Count based on Opponent Health. Pentakill: On Resolve, you deal (200 x Total Focus Count) Damage.'
+    'TRAIT': 'Turret Shot: On Focus you deal up to 15% Damage + Turn Count based on OPP HP. Pentakill: On Resolve, you deal (200 x Total Focus Count) Damage.'
 },{
     'NAME': 'Pokemon',
     'EFFECT': 'Regions, Evolutions',
-    'TRAIT': 'Regions: Region items are shared. Evolutions: On Resolve Gain 2x Defense. Mega: Turn >30 grow 500 HP, Gigantomax: Turn >50 grow 1000 HP.'
+    'TRAIT': 'Regions: Region items are shared. Evolutions: On Resolve Gain 2x DEF. Mega: Turn >30 grow 500 HP, Gigantomax: Turn >50 grow 1000 HP.'
 },{
     'NAME': 'Digimon',
     'EFFECT': 'Digivolve, MegaDigivolution',
-    'TRAIT': 'Digivolve: On Focus you Resolve gain 50% ATK and DEF.  Mega Digivolution : If turn count <5 Double ATK and DEF increase Health by 500.'
+    'TRAIT': 'Digivolve: On Focus you Resolve gain 50% ATK and DEF.  Mega Digivolution : If turn count <5 Double ATK and DEF increase HP by 500.'
 },{
     'NAME': 'Naruto',
     'EFFECT': 'Substitution, Hashirama Cells',
@@ -21,15 +21,15 @@ traits = [{
 },{
     'NAME': 'Chainsawman',
     'EFFECT': 'Devilization, Fearful',
-    'TRAIT': 'Devilization : When Health reaches 50% gain Double ATK, Double DEF, and Double Max Health. Fearful : Strong Fear Infliction.'
+    'TRAIT': 'Devilization : When HP reaches 50% gain Double ATK, Double DEF, and Double Max HP. Fearful : Strong Fear Infliction.'
 },{
     'NAME': 'Bleach',
     'EFFECT': 'Bankai, Spiritual Pressure',
-    'TRAIT': 'Bankai : On Resolve, Gain 2x Attack. Spiritual Pressure : On Block, Defend then Deal Damage equal to your Basic Attack.'
+    'TRAIT': 'Bankai : On Resolve, Gain 2x ATK. Spiritual Pressure : On Block, Defend then Deal Damage equal to your Basic ATK.'
 },{
-    'NAME': 'Attack On Titan',
+    'NAME': 'ATK On Titan',
     'EFFECT': 'Rally, Titan Mode',
-    'TRAIT': 'Rally : On Block, Create (100 * Card Tier) Health. Titan Mode : Gain (200 x Your Focus Count) worth of Health upon Resolve.'
+    'TRAIT': 'Rally : On Block, Create (100 * Card Tier) HP. Titan Mode : Gain (200 x Your Focus Count) worth of HP upon Resolve.'
 },{
     'NAME': 'Black Clover',
     'EFFECT': 'Mana Zone, Grimoire',
@@ -41,11 +41,11 @@ traits = [{
 },{
     'NAME': 'Demon Slayer',
     'EFFECT': 'Total Concentration Breathing, Constant',
-    'TRAIT': 'Breathing : Gain 40% of Opponent HP as your own. Constant :  On Resolve, Your ATK & DEF = Opp ATK and DEF.'
+    'TRAIT': 'Breathing : Gain 40% of OPP HP as your own. Constant :  On Resolve, Your ATK & DEF = Opp ATK and DEF.'
 },{
     'NAME': 'God Of War',
     'EFFECT': 'Blood Orb, Ascension',
-    'TRAIT': ' You can Resolve twice. Blood Orb : 1st Resolve Heal 1/2 of Max Health. Ascension : 2nd Resolve Heal Max Health'
+    'TRAIT': ' You can Resolve twice. Blood Orb : 1st Resolve Heal 1/2 of Max HP. Ascension : 2nd Resolve Heal Max HP'
 },{
     'NAME': '7ds',
     'EFFECT': 'Increase Power, Power Of Friendship',
@@ -53,43 +53,43 @@ traits = [{
 },{
     'NAME': 'One Punch Man',
     'EFFECT': 'Hero Reinforcement, Hero Rankings',
-    'TRAIT': 'Hero Rankings : On Focus gain (15 * Card Tier) AP. Hero Reinforcement : On Opp Focus you create (100 * Card Tier) Health. '
+    'TRAIT': 'Hero Rankings : On Focus gain (15 * Card Tier) AP. Hero Reinforcement : On Opp Focus you create (100 * Card Tier) HP. '
 },{
     'NAME': 'Solo Leveling',
     'EFFECT': 'Rulers Authority, Arise!',
-    'TRAIT': 'Rulers Authority : On Focus, opponent loses (50 + turn) Defense Your Summon gain Ap eqal to value. Arise! : When Opponent Arm Breaks it revives as your Arm.'
+    'TRAIT': 'Rulers Authority : On Focus, OPP loses (50 + turn) DEF Your Summon gain Ap eqal to value. Arise! : When OPP Arm Breaks it revives as your Arm.'
 },{
     'NAME': 'Souls',
     'EFFECT': 'Combo Recognition, Second Phase!',
-    'TRAIT': 'Combo Recognition : :First Phase! : On opponent Focus, gain (100 + turn) ATK. Second Phase! : On Resolve, BASIC = SPECIAL, and SPECIAL = ULTIMATE, Dbl Strike with Basic Attack'
+    'TRAIT': 'Combo Recognition : :First Phase! : On OPP Focus, gain (100 + turn) ATK. Second Phase! : On Resolve, Shuffle Moveset, Dbl Strike with Basic ATK'
 },{
     'NAME': 'Death Note',
     'EFFECT': 'Scheduled Death, Shinigami Eyes',
-    'TRAIT': 'Scheduled Death :First Focus After (130 + (10 * Tier)) Turns You Win. Shinigami Eyes : On Block, Sacrifice 5% Max Health to Increase Turn by (3 + Card Tier).'
+    'TRAIT': 'Scheduled Death :First Focus After (130 + (10 * Tier)) Turns You Win. Shinigami Eyes : On Block, Sacrifice 5% Max HP to Increase Turn by (3 + Card Tier).'
 },{
     'NAME': 'Dragon Ball Z',
     'EFFECT': 'Final Stand, Saiyan Spirit',
-    'TRAIT': 'Final Stand :  When you die, Resolve with 75% ATK + DEF as Health. Saiyan Spirit :  On Focus, Create for (5 * Opponent Stamina) + turn count Health.'
+    'TRAIT': 'Final Stand :  When you die, Resolve with 75% ATK + DEF as HP. Saiyan Spirit :  On Focus, Create for (5 * OPP Stamina) + turn count HP.'
 },{
     'NAME': 'Persona',
     'EFFECT': 'Summon Persona, Summoners Battle',
-    'TRAIT': 'Summon Persona : Summon also deals Damage equal to your Basic Attack. Summoners Battle : Summoning disables Opp summon. '
+    'TRAIT': 'Summon Persona : Summon also deals Damage equal to your Basic ATK. Summoners Battle : Summoning disables Opp summon. '
 },{
     'NAME': 'YuYu Hakusho',
     'EFFECT': 'Spirit Resolved!',
-    'TRAIT': 'Spirit Resolved : Upon Resolve, Gain 2x ATK and all AP is doubled, but defense remains at 100 for remainder of the match.'
+    'TRAIT': 'Spirit Resolved : Upon Resolve, Gain 2x ATK and all AP is doubled, but DEF Locks at 100.'
 },{
     'NAME': 'One Piece',
     'EFFECT': 'Haki : Tier Based Trait',
     'TRAIT': 'Observation: 1-7: 40% DMG Reduction. Armament: 4-7: 2x Focus. Conquerors: 6-7 Decrease OPP AP by 150'
 },{
     'NAME': 'Crown Rift Slayers',
-    'EFFECT': 'Feint Attack, Spiritual Weapons',
-    'TRAIT': 'Feint Attack : If you miss, Crit! Spiritual Weapons : Can use Arms from all Uninverses'
+    'EFFECT': 'Feint ATK, Spiritual Weapons',
+    'TRAIT': 'Feint ATK : If you miss, Crit! Spiritual Weapons : Can use Arms from all Uninverses'
 },{
     'NAME': 'Crown Rift Madness',
     'EFFECT': 'Madness, Beast Blood',
-    'TRAIT': 'Madness : Do not lose turn on Focus, but also do not Heal. Beast Blood : Continue to Gain ATK and DEF after Resolved'
+    'TRAIT': 'Madness : Do not lose turn on Focus, but do not Heal. Beast Blood : Continue to Gain ATK and DEF after Resolved'
 },{
     'NAME': 'Crown Rift Awakening',
     'EFFECT': 'Blood Awakening, Bloodlines',
