0ba5798b92ead1a42a25f9e18a583b79b744fd23
Alex Tillman <tillmanholdingsllc@gmail.com>
2023-04-11 15:03:23 -0400

    Update card_class.py

--- cogs/classes/card_class.py
+++ cogs/classes/card_class.py
@@ -2837,6 +2837,7 @@ class Card:
                 battle_config.next_turn()
             
             if self.is_tank:
+                self._shield_active = True
                 self._shield_value = self._shield_value + (self.tier * 500)
                 battle_config.add_to_battle_log(f"({crown_utilities.class_emojis['TANK']}) {self.name} gained **{self._shield_value}** Shield!")
 
