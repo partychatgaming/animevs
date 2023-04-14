7f8ec4ff0d23a3d5ee58674726ab1514405d9060
Alex Tillman <tillmanholdingsllc@gmail.com>
2023-04-14 07:17:46 -0400

    Water Update
    Changed water element

--- cogs/classes/card_class.py
+++ cogs/classes/card_class.py
@@ -149,6 +149,7 @@ class Card:
             self.freeze_enh = False
             self.ice_counter = 0
             self.water_buff = 0
+            self.water_mult = 1
             self.shock_buff = 0
             self.psychic_debuff = 0
             self.bleed_damage_counter = 0
@@ -3643,17 +3644,18 @@ class Card:
                 self.ultimate_water_buff = self.ultimate_water_buff + 100
             self.water_buff = self.water_buff + 100
             opponent_card.health = opponent_card.health - dmg['DMG']
-            if self.water_buff >=500:
-                opponent_card.health = opponent_card.health - 500
+            if self.water_buff >= (500 * self.water_mult):
+                self.water_mult += 1
+                opponent_card.health = opponent_card.health - (500 * self.water_mult)
                 battle_config.add_to_battle_log(f"{name} {dmg['MESSAGE']}\n*Tsunami Strikes! +{self.water_buff} Dealt!*")
                 self.water_buff = 0
                 if self.move1_element == "WATER":
-                    self.basic_water_buff = self.basic_water_buff - 500
+                    self.basic_water_buff = self.basic_water_buff - (500 * self.water_mult)
                 if self.move2_element == "WATER":
-                    self.special_water_buff = self.special_water_buff - 500
+                    self.special_water_buff = self.special_water_buff - (500 * self.water_mult)
                 if self.move3_element == "WATER":
-                    self.ultimate_water_buff = self.ultimate_water_buff - 500
-            elif self.water_buff >= 400:
+                    self.ultimate_water_buff = self.ultimate_water_buff - (500 * self.water_mult)
+            elif self.water_buff >= ((500 * self.water_mult) - 100):
                 battle_config.add_to_battle_log(f"{name} {dmg['MESSAGE']}\n*Tsunami Incoming... +{self.water_buff}*")
             else:
                 battle_config.add_to_battle_log(f"{name} {dmg['MESSAGE']}\n*The Tide Stirs +{self.water_buff}*")
